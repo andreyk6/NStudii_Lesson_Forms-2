99cf541d2c1593ad9578b2702ba9b7444db962a8
GundamMster <gundammster@gmail.com>
2017-01-21 13:10:00 +0200

    Add StoreIndexPAge

---
+++ EmptyProject/Controllers/StoreController.cs
@@ -0,0 +1,22 @@
+﻿using EmptyProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmptyProject.Controllers
+{
+    public class StoreController : Controller
+    {
+        ApplicationContext _db = new ApplicationContext();
+        // GET: Store
+
+        public ActionResult Index()
+        {
+            return View(_db.Store.ToList());
+        }
+        
+
+    }
+}
