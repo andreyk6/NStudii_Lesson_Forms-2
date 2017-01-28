a5e6aa329f25d4f6ff9df9a2d7ed8e14497b4a93
GundamMster <gundammster@gmail.com>
2017-01-28 13:59:38 +0200

    Create Store Controller

---
+++ EmptyProject/Controllers/StoreController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmptyProject.Models;
+using EmptyProject.ViewModels.Store;
+
+namespace EmptyProject.Controllers
+{
+    public class StoreController : Controller
+    {
+        ApplicationContext _db = new ApplicationContext();
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View(_db.Store.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new CreateStoreVM());
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateStoreVM storeVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var store = new Store()
+                {
+                    Name = storeVM.Name,
+                    Description = storeVM.Description,
+                    BrandId = storeVM.BrandId
+                };
+                _db.Store.Add(store);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(storeVM);
+            }
+        }
+
+    }
+}
---
+++ EmptyProject/ViewModels/Store/CreateStoreVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmptyProject.ViewModels.Store
+{
+    public class CreateStoreVM
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(10)]
+        public string Description { get; set; }
+        [Required]
+        public Guid BrandId { get; set; }
+    }
+}
