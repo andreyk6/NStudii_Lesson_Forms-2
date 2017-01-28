a569b92845aa6e712ada17081924852acb3cdfb7
andrii.konovalenko <andreyk6@gmail.com>
2017-01-28 13:57:18 +0200

    Item Controller (Ivan)

---
+++ EmptyProject/Controllers/ItemsController.cs
@@ -0,0 +1,54 @@
+﻿using EmptyProject.Models;
+using EmptyProject.ViewModels.Item;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmptyProject.Controllers
+{
+    public class ItemsController : Controller
+    {
+        // GET: Items
+        ApplicationContext db = new ApplicationContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new CreateItemVM());
+        }
+        [HttpPost]
+        public ActionResult Create(CreateItemVM item)
+        {
+            if(ModelState.IsValid)
+            {
+                var NewItem = new Models.Item()
+                {
+                    Name = item.Name,
+                    Description=item.Description,
+                    ImageURL=item.ImageUrl,
+                    Price=item.Price,
+                    StoreId=item.StoreId
+                };
+                db.Item.Add(NewItem);
+                db.SaveChanges();
+
+                return RedirectToAction("ItemInfo", new { Id = NewItem.Id });
+            }
+            return View(item);
+        }
+
+        public ActionResult Edit(Guid Id)
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ EmptyProject/ViewModels/Item/CreateItemVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmptyProject.ViewModels.Item
+{
+    public class CreateItemVM
+    {
+        [Required]
+        public string Name { get; set; }
+
+        //[Required]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0.01,99999999999)]
+        public float Price { get; set; }
+
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public Guid StoreId { get; set; }
+    }
+}
