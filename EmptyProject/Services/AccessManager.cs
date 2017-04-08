51fee1b3d4f0cb86bb8776be94b96459f50a9fa8
13 <13@DESKTOP-RM101FV>
2017-04-08 13:33:59 +0300

    Access manager

--- EmptyProject/Services/AccessManager.cs
+++ EmptyProject/Services/AccessManager.cs
@@ -1,11 +1,62 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using EmptyProject.Models;
 
 namespace EmptyProject.Services
 {
-    public class AccessManager
+    public static class AccessManager
     {
-       bool isAuth
-    }*/
+        private static ApplicationContext _db;
+
+        static AccessManager()
+        {
+            _db = new ApplicationContext();
+        }
+
+        public static bool IsAuthorized()
+        {
+            var user = GetCurrentUser();
+            if (user == null)
+            {
+                return false;
+            }
+
+            if (user.Token.ExpiresDate <= DateTime.Now)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static Token GetToken()
+        {
+            System.Web.HttpCookie tokenCookies = HttpContext.Current.Request.Cookies.Get("token");
+            if (tokenCookies == null)
+            {
+                return null;
+            }
+            string tokenValue = tokenCookies.Value;
+            if (tokenValue == null)
+            {
+                return null;
+            }
+            Token token = _db.Tokens.FirstOrDefault(t => t.Value == tokenValue);
+            if (token == null)
+            {
+                return null;
+            }
+            return token;
+        }
+
+        public static User GetCurrentUser()
+        {
+            var token = GetToken();
+            if (token == null)
+                return null;
+
+            return token.User;
+        }
+    }
+}
